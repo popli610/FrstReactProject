945bf36ee45bf516668af7a903281424ea096e16
popli610 <95101617+popli610@users.noreply.github.com>
2022-03-20 10:26:06 +0530

    Updates

---
+++ precache-manifest.eafd86aa44f2fc6cf9f1a0e28ab7032b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68240b3c8c2191438d72",
+    "url": "/FrstReactProject/static/css/main.67f6e514.chunk.css"
+  },
+  {
+    "revision": "68240b3c8c2191438d72",
+    "url": "/FrstReactProject/static/js/main.68240b3c.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/FrstReactProject/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/FrstReactProject/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "28e56ad9667a2ad2d81c",
+    "url": "/FrstReactProject/static/js/runtime~main.28e56ad9.js"
+  },
+  {
+    "revision": "4bd0184d60e3d26b002bcc0fb0488621",
+    "url": "/FrstReactProject/index.html"
+  }
+];
